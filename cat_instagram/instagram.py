ab3cf8549992baf5a31fda8baa71f80c05d45374
AmaSus01 <glum1419@gmail.com>
2021-09-05 17:01:42 +0300

    new code vebe

--- cat_instagram/instagram.py
+++ cat_instagram/instagram.py
@@ -14,40 +14,34 @@ if args['user']:
 if args['post']:
     api.post_info(username=args["user"])
 
-print('If you want to get some location info, just press Y/y or if not press N/n')
-a = input()
 
-if a == 'Y' or 'y':
-    print('Write place where target took photo: ')
-    geoTag = input()
-    locator = geopy.Nominatim(user_agent='myGeocoder')
-    location = locator.geocode(geoTag)
-
-    #print("Latitude={}, Longitude={}".format(location.latitude, location.longitude))
-    map_access_token = open(".mapbox_token").read()
-
-    fig = go.Figure(go.Scattermapbox(
-        lat=[location.latitude],
-        lon=[location.longitude],
-        mode='markers',
-        marker=go.scattermapbox.Marker(size=14),
-        text=['Target'],
-    ))
-
-    fig.update_layout(
-        hovermode='closest',
-        mapbox=dict(
-            accesstoken=map_access_token,
-            bearing=0,
-            center=go.layout.mapbox.Center(
-                lat=int(location.latitude),
-                lon=int(location.longitude)
-            ),
-            pitch=0,
-            zoom=5
-        )
-    )
-    fig.show()
-else:
-    print('ok')
-    exit(1)
+# def get_geolocation():
+#     geoTag = input()
+#     locator = geopy.Nominatim(user_agent='myGeocoder')
+#     location = locator.geocode(geoTag)
+#
+#     #print("Latitude={}, Longitude={}".format(location.latitude, location.longitude))
+#     map_access_token = open(".mapbox_token").read()
+#
+#     fig = go.Figure(go.Scattermapbox(
+#         lat=[location.latitude],
+#         lon=[location.longitude],
+#         mode='markers',
+#         marker=go.scattermapbox.Marker(size=14),
+#         text=['Target'],
+#     ))
+#
+#     fig.update_layout(
+#         hovermode='closest',
+#         mapbox=dict(
+#             accesstoken=map_access_token,
+#             bearing=0,
+#             center=go.layout.mapbox.Center(
+#                 lat=int(location.latitude),
+#                 lon=int(location.longitude)
+#             ),
+#             pitch=0,
+#             zoom=5
+#         )
+#     )
+#     fig.show()
